771c7a112d36569d878a059f480455f951ca6d5f
angryshan <angryshan@qq.com>
2026-08-17 18:21:52 +0800

    第二次提交

--- app/models/log_document.go
+++ app/models/log_document.go
@@ -2,18 +2,20 @@ package models
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/phachon/mm-wiki/app/utils"
 	"github.com/snail007/go-activerecord/mysql"
-	"time"
 )
 
 const (
-	LogDocument_Action_Create   = 1
-	LogDocument_Action_Update   = 2
-	LogDocument_Action_Delete   = 3
-	LogDocument_Action_Recover  = 4 // 回收站恢复文档
-	LogDocument_Action_PermDel  = 5 // 彻底删除文档
-	LogDocument_Action_Clear   = 6 // 清空回收站
+	LogDocument_Action_Create  = 1
+	LogDocument_Action_Update  = 2
+	LogDocument_Action_Delete  = 3
+	LogDocument_Action_Read    = 4
+	LogDocument_Action_Recover = 5 // 回收站恢复文档
+	LogDocument_Action_PermDel = 6 // 彻底删除文档
+	LogDocument_Action_Clear   = 7 // 清空回收站
 )
 
 const Table_LogDocument_Name = "log_document"
@@ -83,6 +85,36 @@ func (ld *LogDocument) DeleteAction(userId string, documentId string, spaceId st
 	return ld.Insert(logDocument)
 }
 
+func (ld *LogDocument) ReadAction(userId string, documentId string, spaceId string) (id int64, err error) {
+
+	//十分钟内重复阅读不做入库
+	db := G.DB()
+	var rs *mysql.ResultSet
+	rs, err = db.Query(db.AR().Select("count(*) as count").From(Table_LogDocument_Name).Where(map[string]interface{}{
+		"document_id":    documentId,
+		"user_id":        userId,
+		"action":         LogDocument_Action_Read,
+		"create_time >=": time.Now().Unix() - 600,
+	}))
+	if err != nil {
+		return
+	}
+	count := utils.Convert.StringToInt64(rs.Value("count"))
+
+	if count < 1 {
+		logDocument := map[string]interface{}{
+			"user_id":     userId,
+			"document_id": documentId,
+			"space_id":    spaceId,
+			"comment":     "阅读了文档",
+			"action":      LogDocument_Action_Read,
+			"create_time": time.Now().Unix(),
+		}
+		return ld.Insert(logDocument)
+	}
+	return
+}
+
 func (ld *LogDocument) RecoverAction(userId string, documentId string, spaceId string) (id int64, err error) {
 	logDocument := map[string]interface{}{
 		"user_id":     userId,
@@ -145,16 +177,42 @@ func (ld *LogDocument) GetLogDocumentsByUserId(userId string) (logDocuments []ma
 	return
 }
 
-func (ld *LogDocument) GetLogDocumentsByDocumentIdAndLimit(documentId string, limit int, number int) (logDocuments []map[string]string, err error) {
+func (ld *LogDocument) GetLogDocumentsByDocumentIdAndLimit(documentId string, limit int, number int, logReadActionStatus bool) (logDocuments []map[string]string, err error) {
 	db := G.DB()
 	var rs *mysql.ResultSet
-	rs, err = db.Query(db.AR().From(Table_LogDocument_Name).Where(map[string]interface{}{
+
+	where := map[string]interface{}{
 		"document_id": documentId,
-	}).Limit(limit, number).OrderBy("log_document_id", "DESC"))
+	}
+	if logReadActionStatus {
+		where["action"] = LogDocument_Action_Read
+	} else {
+		where["action !="] = LogDocument_Action_Read
+	}
+
+	rs, err = db.Query(db.AR().From(Table_LogDocument_Name).Where(where).Limit(limit, number).OrderBy("log_document_id", "DESC"))
 	if err != nil {
 		return
 	}
 	logDocuments = rs.Rows()
+
+	return
+}
+
+func (ld *LogDocument) GetLogDocumentLastUpdateByDocumentId(documentId string) (logDocuments map[string]string, err error) {
+	db := G.DB()
+	var rs *mysql.ResultSet
+
+	where := map[string]interface{}{
+		"document_id": documentId,
+	}
+	where["action"] = LogDocument_Action_Update
+
+	rs, err = db.Query(db.AR().From(Table_LogDocument_Name).Where(where).OrderBy("log_document_id", "DESC"))
+	if err != nil {
+		return
+	}
+	logDocuments = rs.Row()
 
 	return
 }
@@ -194,6 +252,60 @@ func (ld *LogDocument) GetLogDocumentsByKeywordAndLimit(keyword string, limit in
 	rs, err = db.Query(db.AR().From(Table_LogDocument_Name).Where(map[string]interface{}{
 		"comment LIKE": "%" + keyword + "%",
 	}).Limit(limit, number).OrderBy("log_document_id", "DESC"))
+	if err != nil {
+		return
+	}
+	logDocuments = rs.Rows()
+
+	return
+}
+
+func (ld *LogDocument) GetDefaultLogDocumentsByLimit(userId string, limit int, number int, limitUser bool) (logDocuments []map[string]string, err error) {
+	db := G.DB()
+	var rs *mysql.ResultSet
+	where := db.AR().From(Table_LogDocument_Name)
+
+	var speaceIds []string
+
+	// 查询用户空间权限
+	spaceUserRs, err := db.Query(db.AR().From(Table_SpaceUser_Name).Where(map[string]interface{}{
+		"user_id": userId,
+	}))
+
+	spaceUsers := spaceUserRs.Rows()
+	spaceUsersLen := len(spaceUsers)
+	for i := 0; i < spaceUsersLen; i++ {
+		spaceUser := spaceUsers[i]
+		speaceIds = append(speaceIds, spaceUser["space_id"])
+	}
+
+	// 查询公共空间
+	spaceRs, err := db.Query(db.AR().From(Table_Space_Name).Where(map[string]interface{}{
+		"visit_level": "public",
+	}))
+
+	spaces := spaceRs.Rows()
+	for i := 0; i < len(spaces); i++ {
+		space := spaces[i]
+		speaceIds = append(speaceIds, space["space_id"])
+	}
+
+	where.WhereWrap(map[string]interface{}{
+		"action != ": LogDocument_Action_Read,
+	}, "", " and ")
+
+	if limitUser == true {
+		where.WhereWrap(map[string]interface{}{
+			"user_id": userId,
+		}, "", " and ")
+	}
+
+	where.WhereWrap(map[string]interface{}{
+		"space_id": speaceIds,
+	}, "", "")
+
+	rs, err = db.Query(where.Limit(limit, number).OrderBy("log_document_id", "DESC"))
+
 	if err != nil {
 		return
 	}
@@ -257,17 +369,61 @@ func (ld *LogDocument) GetLogDocumentsByLimit(userId string, limit int, number i
 	return
 }
 
-func (ld *LogDocument) CountLogDocumentsByDocumentId(documentId string) (count int64, err error) {
+func (ld *LogDocument) CountLogDocumentsByDocumentId(documentId string, logReadActionStatus bool) (count int64, err error) {
 
 	db := G.DB()
 	var rs *mysql.ResultSet
-	rs, err = db.Query(db.AR().Select("count(*) as total").From(Table_LogDocument_Name).Where(map[string]interface{}{
+
+	where := map[string]interface{}{
 		"document_id": documentId,
-	}))
+	}
+	if logReadActionStatus {
+		where["action"] = LogDocument_Action_Read
+	} else {
+		where["action !="] = LogDocument_Action_Read
+	}
+
+	rs, err = db.Query(db.AR().Select("count(*) as total").From(Table_LogDocument_Name).Where(where))
 	if err != nil {
 		return
 	}
 	count = utils.Convert.StringToInt64(rs.Value("total"))
+	return
+}
+
+func (ld *LogDocument) CountReadActionLogDocumentsByDocumentId(documentId string) (logReadActionCounts map[string]interface{}, err error) {
+
+	db := G.DB()
+	var rs *mysql.ResultSet
+	rs, err = db.Query(db.AR().Select("count(*) as total, COUNT(DISTINCT user_id) as num").From(Table_LogDocument_Name).Where(map[string]interface{}{
+		"document_id": documentId,
+		"action":      LogDocument_Action_Read,
+	}))
+	if err != nil {
+		return
+	}
+	count := utils.Convert.StringToInt64(rs.Value("total"))
+	num := utils.Convert.StringToInt64(rs.Value("num"))
+
+	var rs7day *mysql.ResultSet
+	rs7day, err = db.Query(db.AR().Select("count(*) as total, COUNT(DISTINCT user_id) as num").From(Table_LogDocument_Name).Where(map[string]interface{}{
+		"document_id":    documentId,
+		"action":         LogDocument_Action_Read,
+		"create_time >=": time.Now().Unix() - (7 * 86400),
+	}))
+	if err != nil {
+		return
+	}
+
+	count7day := utils.Convert.StringToInt64(rs7day.Value("total"))
+	num7day := utils.Convert.StringToInt64(rs7day.Value("num"))
+
+	logReadActionCounts = map[string]interface{}{
+		"count":     count,
+		"num":       num,
+		"count7day": count7day,
+		"num7day":   num7day,
+	}
 	return
 }
 
